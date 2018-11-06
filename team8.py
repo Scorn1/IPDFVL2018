5be7b72fc809e365b3b8fc32c939c362d270e31e
Stephen Cornelius <corneste000@students.fvlhs.org>
2018-11-06 19:09:06 +0000

    added team 8 code

--- team8.py
+++ team8.py
@@ -3,66 +3,93 @@
 #     team_name: a string
 #     strategy_name: a string
 #     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
+#     move: A function that returns 'C' or 'B'
 ####
 
 team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Losing is not a word in my vocabulary'
+strategy_description = 'Guess and Check till an answer makes itself evident'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+    if len(my_history) == 0:
+        return 'b'
+    elif len(my_history) == 1:
+        return 'b'
+    elif len(my_history) == 2:
+        return 'c'
+    elif len(my_history) >= 3:    
+        if their_history[len(their_history)-3:len(their_history)].upper() == 'CCB':
+            return 'b'
+        elif their_history[len(their_history)-3:len(their_history)].upper() == 'CBC':
+            return 'b'
+        elif their_history[len(their_history)-3:len(their_history)].upper() == 'BCC':
+            return 'b'
+        elif their_history[len(their_history)-3:len(their_history)].upper() == 'CBB':
+            return 'c'
+        elif their_history[len(their_history)-3:len(their_history)].upper() == 'BCB':
+            return 'c'
+        elif their_history[len(their_history)-3:len(their_history)].upper() == 'BBC':
+            return 'c'
+        elif their_history[len(their_history)-3:len(their_history)].upper() == 'CCC':
+            return 'c'
+        elif their_history[len(their_history)-3:len(their_history)].upper() == 'BBB':
+            return 'b'
+    elif their_history[-1:].upper() != 'C':
+        if their_history[-1].upper() != 'B':
+            return 'b'
+    elif len(my_history)%6 ==0:
+        return ' '
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
-def test_move(my_history, their_history, my_score, their_score, result):
+def test_move(my_history, their_history, my_score, their_score, result):     # if their_history[len(their_history)-3:len(their_history)].upper() == 'CCC'
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
     Returns True or False, dpending on whether result was as expected.
     '''
     real_result = move(my_history, their_history, my_score, their_score)
-    if real_result == result:
-        return True
-    else:
-        print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+
-            ") returned " + "'" + real_result + "'" +
-            " and should have returned '" + result + "'")
-        return False
-
-if __name__ == '__main__':
-     
-    # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,
-              their_score=0,
-              result='b'):
-         print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
-              my_score=0, 
-              their_score=0,
-              result='b')             
+    a = 0
+    
+    if len(my_history) == 0:
+        return 'b'
+    elif len(my_history) == 1:
+        return 'b'
+    elif len(my_history) == 2:
+        return 'c'
+    elif len(my_history) >= 3:    
+        if their_history[-1].upper() == 'C':
+            if their_history[-2].upper() == 'C':
+                if their_history[-3].upper() == 'B':
+                    return 'c'
+        elif their_history[-1].upper() == 'C':
+            if their_history[-2].upper() == 'B':
+                if their_history[-3].upper() == 'C':
+                    return 'b'
+        elif their_history[-1].upper() == 'B':
+            if their_history[-2].upper() == 'C':
+                if their_history[-3].upper() == 'C':
+                    return 'c'
+        elif their_history[-1].upper() == 'C':
+           if their_history[-2].upper() == 'B':
+                if their_history[-3].upper() == 'B':
+                    return 'c'
+        elif their_history[-1].upper() == 'B':
+            if their_history[-2].upper() == 'C':
+                if their_history[-3].upper() == 'B':
+                    return 'b'
+        elif their_history[-1].upper() == 'B':
+            if their_history[-2].upper() == 'B':
+                if their_history[-3].upper() == 'C':
+                    return 'c'
+        elif their_history[-1].upper() == 'C':
+            if their_history[-2].upper() == 'C':
+                if their_history[-3].upper() == 'C':
+                    return 'c'
+        elif their_history[-1].upper() == 'B':
+            if their_history[-2].upper() == 'B':
+                if their_history[-3].upper() == 'B':
+                    return 'b'
+    elif their_history[-1:].upper() != 'C':
+        if their_history[-1].upper() != 'B':
+            return 'b'
+    elif len(my_history)%6 ==0:
+        return ' '
